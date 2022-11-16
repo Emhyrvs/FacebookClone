7b80756215b7857aed88795c525487bada48f911
maczak2 <maciejzakrzewski1998@gmail.com>
2022-11-16 15:35:34 +0100

    Add project files.

---
+++ FacebookClone/Models/Profile.cs
@@ -0,0 +1,13 @@
+﻿namespace FacebookClone.Models
+{
+    public class Profile
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public  DateTimeOffset BrithDate { get; set; }
+    }
+}
